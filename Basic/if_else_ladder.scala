0b7f146767f79c0c8938cb53992dc4e37d0a0d1c
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-13 22:50:57 +0530

    Create if_else_ladder.scala

---
+++ Basic/if_else_ladder.scala
@@ -0,0 +1,10 @@
+var  resume:String ="selected not"
+if( resume == "selected" ){
+  println("Welcome to Status Neo")
+} else if( resume == "not selected" ){
+  println("Sorry, not selected")
+} else if( resume == "in process" ){
+  println("waiting....")
+} else{
+  println("This is else statement")
+}
