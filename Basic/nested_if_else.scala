4c2c478d90fed1db533ebdaadc52c921ff4d90f1
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-13 22:50:24 +0530

    Create nested_if_else.scala

---
+++ Basic/nested_if_else.scala
@@ -0,0 +1,10 @@
+var Resume:String = "selected"
+var interview:String = "not clear"
+if(Resume == "selected") {
+  println ("Welcome to Status Neo")
+  if(interview == "clear")
+    println("congratulation,you are selected!")
+  else
+    println("Sorry,not selected")
+} else
+  println("Sorry,not selected")
