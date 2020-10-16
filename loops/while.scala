29eee4e12ec62afd9fbe9167be0c09df69faf10a
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-16 23:24:07 +0530

    Create while.scala

---
+++ loops/while.scala
@@ -0,0 +1,11 @@
+//while loop
+object loops {
+  def main(args: Array[String]) {
+    var a = 2;                       // Initialization
+    println("Table of 2")
+    while( a<=20 ){                // Condition
+      println("Status Neo: "+a)
+      a = a+2                        // Incrementation
+    }
+  }
+}
