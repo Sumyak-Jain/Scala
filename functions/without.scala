481f922835356ab18a7a7d7a78ede31dcfaa9cdc
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-18 22:57:19 +0530

    Create without.scala

---
+++ functions/without.scala
@@ -0,0 +1,9 @@
+//Scala Function Example without using = Operator
+object functions {
+  def main(args: Array[String]) {
+    WithouEqualTo()           // Calling function
+  }
+  def WithouEqualTo()  {        // Defining a function
+    println("Scala Function Example without using = Operator")
+  }
+}
