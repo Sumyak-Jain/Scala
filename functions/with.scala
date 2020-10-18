48a2513053be08791ab66dfc2c02c547645eff70
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-18 22:57:55 +0530

    Create with.scala

---
+++ functions/with.scala
@@ -0,0 +1,10 @@
+//Scala Function Example with = Operator
+object functions {
+  def main(args: Array[String]) {
+    println(WithEqualTo()) // Calling function
+  }
+  def WithEqualTo(): String = { // Defining a function
+    val x = "Scala Function Example with = Operator"
+    x
+  }
+}
