93d4550ba5752a52a61011206514f0d4b6c6c6bf
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-23 23:38:06 +0530

    Create substring.scala

---
+++ String/substring.scala
@@ -0,0 +1,15 @@
+//Scala substring() Method 
+class substring{
+  var s1 = "welcome to status neo"
+  def print(){
+    println("Original string is: "+"welcome to status neo")
+    println("Substring is: "+ s1.substring(11,21))
+  }
+}
+
+object string{
+  def main(args:Array[String]){
+    var a = new equal()
+    a.print()
+  }
+}
