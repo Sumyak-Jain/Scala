d2c178735a1a5e0ae910b912e106434a64c6054a
SUMYAK JAIN <3456gaurav@gmail.com>
2020-10-23 23:36:43 +0530

    Create compare.scala

---
+++ String/compare.scala
@@ -0,0 +1,17 @@
+//Scala compareTo() Method 
+class compare{
+  var s1 = "welcome to status neo"
+  var s2 = "status neo"
+  var s3 = "status neo"
+  def print(){
+    println(s2.compareTo(s3))
+    println(s1.compareTo(s2))
+  }
+}
+
+object string{
+  def main(args:Array[String]){
+    var a = new equal()
+    a.print()
+  }
+}
